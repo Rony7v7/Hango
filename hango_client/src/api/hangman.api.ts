66e37f1ed63b277fbf94ee6446db5dcf98b288ba
Rony Ordoñez <ronyfarido@gmail.com>
2024-07-03 21:01:07 -0500

    Updated astro config and ui

---
+++ hango_client/src/api/hangman.api.ts
@@ -0,0 +1,7 @@
+// const URL_BASE = 'http://localhost:3000/hangman/api/v1/';
+
+// export const fetchWord = async () => {
+//   const response = await fetch(`${URL_BASE}new_game`);
+//   return response.json();
+// };
+
