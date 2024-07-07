1446c57fcd23ece0e014f0ebe51cbe0a40bd939b
Rony Ordoñez <ronyfarido@gmail.com>
2024-07-07 16:18:42 -0500

    Added enpoints

--- hango_client/src/api/hangman.api.ts
+++ hango_client/src/api/hangman.api.ts
@@ -1,7 +1,24 @@
-// const URL_BASE = 'http://localhost:3000/hangman/api/v1/';
+const API_URL = 'http://localhost:8000/hangman/api/v1/';
 
-// export const fetchWord = async () => {
-//   const response = await fetch(`${URL_BASE}new_game`);
-//   return response.json();
-// };
+export async function getGameData(id: string) {
+    const res = await fetch(`${API_URL}hangman/${id}`);
+    const data = await res.json();
+    return {id: data.id, word: data.word, guesses: data.guesses, status: data.status};
+}
 
+export async function newGame() {
+    const res = await fetch(`${API_URL}new_game`)
+    const data = await res.json();
+    return {id: data.id, word: data.word, guesses: data.guesses, status: data.status};
+}
+
+export async function guessLetter(id:string, letter:string) {
+    const res = await fetch(`${API_URL}guess_letter/${id}/${letter}/`, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+    });
+    const data = await res.json();
+    return data;
+}
