8c85a2acdadf5dbbc12e9627be1c5cdff9279d6b
Rony Ordoñez <ronyfarido@gmail.com>
2024-07-08 12:30:53 -0500

    Better components with Preact

---
+++ hango_client/src/components/WordDisplay.jsx
@@ -0,0 +1,12 @@
+const WordDisplay = ({ word, guessedLetters }) => {
+  
+  const displayWord = word.toUpperCase().split('').map((letter, index) =>
+    <span class="mx-2 text-3xl font-light p-2 rounded-lg bg-black/10 ">
+      {guessedLetters.includes(letter) ? letter : '_'}
+    </span>
+  )
+
+  return <div className="word-display">{displayWord}</div>;
+};
+
+export default WordDisplay;
