5ec51eaa911ca34d7dcf9e0ccabca7e26a77a16a
Rony Ordoñez <ronyfarido@gmail.com>
2024-07-08 12:28:26 -0500

    Updated dynamic gallow

--- hango_client/src/components/Gallow.jsx
+++ hango_client/src/components/Gallow.jsx
@@ -1,20 +1,13 @@
 // Este componente de preact es el encargado de mostrar el ahorcado dinamico en la pantalla.
 // Recibe un parametro que es el intento actual del usuario, y en base a eso muestra un componente de 
-// Astro diferente. Son 7 en total y están ubicados en ../components/gallow
+// Astro diferente. Son 7 en total y están ubicados en /public/img/gallow
 
-import { useEffect, useState } from 'preact/hooks';
-
-import {gallow_1} from './gallow/gallow_2.astro';
-
-const Gallow = ({ attempt }) => {
-
+const Gallow = ({attempts}) => {
     return (
-        <div>
-            {gallow_1}
-        </div>
+        <img src={`/img/gallow/gallow_${attempts}.svg`} alt={`Gallow ${attempts}`} />
     );
-
 }
 
-export default Gallow;
+// hango_client/public/img\gallow\Gallow_1.png
 
+export default Gallow;
