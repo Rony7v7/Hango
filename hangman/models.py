1f922708cc4af1877603da163ffb7e62c5700890
Rony Ordoñez <ronyfarido@gmail.com>
2024-07-08 12:29:00 -0500

    Removed unused attribute

--- hangman/models.py
+++ hangman/models.py
@@ -6,7 +6,6 @@ class Hangman(models.Model):
     word = models.CharField(max_length=50) # The word to be guessed
     attempts = models.IntegerField() # Number of attempts left
     guessed_letters = models.CharField(max_length=50) # Letters that have been guessed
-    game_over = models.BooleanField() # Whether the game is over
     game_won = models.BooleanField()# Whether the game was won
     game_lost = models.BooleanField() # Whether the game was lost
 
