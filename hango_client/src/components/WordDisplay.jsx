fea6c0bdd42c51d12c818a709ae101ac5753ec7f
Rony Ordoñez <ronyfarido@gmail.com>
2024-07-16 15:03:19 -0500

    fixed unused variable

--- hango_client/src/components/WordDisplay.jsx
+++ hango_client/src/components/WordDisplay.jsx
@@ -2,7 +2,7 @@ const WordDisplay = ({ word, guessedLetters }) => {
 
   guessedLetters = guessedLetters.split('').map(letter => letter.toUpperCase());
   
-  const displayWord = word.toUpperCase().split('').map((letter, index) =>
+  const displayWord = word.toUpperCase().split('').map((letter) =>
     <span class="mx-2 text-3xl font-light p-2 rounded-lg bg-black/10 ">
       {guessedLetters.includes(letter) ? letter : '_'}
     </span>
